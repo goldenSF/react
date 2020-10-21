b42eb00a9786be1b201b00cf8e039f7bf8362d51
Kaung Win <tkong7511@yahoo.com>
2020-10-21 01:23:22 -0700

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
@@ -17,16 +18,13 @@ class Main extends Component {
         this.setState({selectedCampsite: campsitId});
     }
 
-    this.render() {
+    render() {
         return (
             <div>
-                <Navbar dar color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">NuCamp</NavbarBrand>
-                    </div>
-                </Navbar>
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
+                <Header />
+                <Directory campsites={this.state.campsites} onClick={campsiteId => this.omCampsiteSelect(campsiteId)} />
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     };
