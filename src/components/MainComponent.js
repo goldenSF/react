cba4a48b2f81041929db4078826680949afcadb7
Kaung Win <tkong7511@yahoo.com>
2020-10-25 21:51:45 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,17 +29,27 @@ class Main extends Component {
                 <Home 
                     campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
                     promotion={this.state.promotion.filter(promotion => promotion.featured[0])}
-                    partner={this.state.partner.filter(partner -> partner.featured)[0]}
+                    partner={this.state.partner.filter(partner => partner.featured)[0]}
                 />
             );
         }
         
+        const CampsiteWidthId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsite.filter(campsite => campsite.id === +match.params.campsiteId[0])}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/ditectory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWidthId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
